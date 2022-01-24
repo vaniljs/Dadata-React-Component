c4db78883e0930606999e6b076ddbdef871d0d55
Vaniljs <a.sorokin59@yandex.ru>
2022-01-24 20:00:02 +0500

    Update Dadata.js
    Add url categories for search

--- Dadata.js
+++ Dadata.js
@@ -7,6 +7,27 @@ export default function App() {
   });
 
   function getData() {
+    
+    // Email https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/email
+    // Адреса https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address
+    // Банки https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/bank
+    // Валюты https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/currency
+    // Кем выдан паспорт https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fms_unit
+    // Марки автомобилей https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/car_brand
+    // Мировые суды https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/region_court
+    // Налоговые инспекции https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fns_unit
+    // ОКВЭД https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/okved2
+    // ОКПД https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/okpd2
+    // ОКТМО https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/oktmo
+    // Организации https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party
+    // Почтовые отделения https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/postal_unit
+    // Станции метро https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/metro
+    // Страны https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/country
+    // Таможни https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fts_unit
+    // Товары и услуги https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/mktu
+    // ФИАС https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/oktmo
+    // ФИО https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio
+    
     const url =
       'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address';
     const options = {
